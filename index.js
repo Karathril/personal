1f1a2b2a2f778abd9c45dfd47dc66ab7e31c692f
brayanMoreno <karathril@gmail.com>
2023-05-10 14:28:12 -0400

    Co-authored-by: brayanMoreno <karathril@gmail.com>

---
+++ index.js
@@ -0,0 +1,40 @@
+var pagina = 1;
+
+$("#enviar").click(function(){
+    $.get("https://rickandmortyapi.com/api/character?page=1", function(data){
+        $.each(data.results, function(i, dato) {
+        $("#datos").append('<li class="list-group-item list-group-item-dark" >' + dato.id + ': ' + dato.name + '</li>');
+        });
+    });
+});
+
+$("#pagina_siguiente").click(function(){
+    pagina++;
+    $.get("https://rickandmortyapi.com/api/character?page=" + pagina, function(data){
+        $("#datos").empty();    
+        $.each(data.results, function(i, dato) {
+            $("#datos").append('<li class="list-group-item list-group-item-dark" >' + dato.id + ': ' + dato.name + '</li>');
+        });
+    });     
+});
+
+$("#pagina_anterior").click(function(){
+    if(pagina==1){
+        pagina=1;
+    }else{
+        pagina--;
+    }
+    
+    $.get("https://rickandmortyapi.com/api/character?page=" + pagina, function(data){
+        $("#datos").empty();    
+        $.each(data.results, function(i, dato) {
+            $("#datos").append('<li class="list-group-item list-group-item-dark" >' + dato.id + ': ' + dato.name + '</li>');
+        });
+    });     
+});
+
+
+
+
+
+
