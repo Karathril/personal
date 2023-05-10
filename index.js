9eba376aa464ab2e38cce0042d27acc9090d353b
brayanMoreno <karathril@gmail.com>
2023-05-10 14:53:17 -0400

    Co-authored-by: brayanMoreno <karathril@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var pagina = 1;
 
 $("#enviar").click(function(){
     $.get("https://rickandmortyapi.com/api/character?page=1", function(data){
+        $("#datos").empty();   
         $.each(data.results, function(i, dato) {
         $("#datos").append('<li class="list-group-item list-group-item-dark" >' + dato.id + ': ' + dato.name + '</li>');
         });
